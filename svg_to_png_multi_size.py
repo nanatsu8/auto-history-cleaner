5d55c34da15a4ed5eaeefa81668ac83ffa14e878
kp8 <natsuki111124@gmail.com>
2025-09-22 00:18:05 +0900

    Chrome拡張機能を作成し、起動時に自動で履歴とダウンロード履歴を削除する機能を実装

---
+++ svg_to_png_multi_size.py
@@ -0,0 +1,22 @@
+import cairosvg
+from PIL import Image
+import os
+
+# 変換したいサイズ
+SIZES = [16, 32, 48, 128]
+SVG_FILE = "icons/icon.svg"  # SVGファイル名
+
+# 一時PNGファイルにまず変換
+TMP_PNG = "tmp_icon.png"
+cairosvg.svg2png(url=SVG_FILE, write_to=TMP_PNG)
+
+# Pillowでリサイズして各サイズのPNGを保存
+with Image.open(TMP_PNG) as img:
+    for size in SIZES:
+        resized_img = img.resize((size, size), Image.LANCZOS)
+        out_name = f"icon{size}.png"
+        resized_img.save(out_name)
+        print(f"Saved {out_name}")
+
+# 一時ファイル削除
+os.remove(TMP_PNG)
